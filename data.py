e33c831a4a81ddfcd83ee612a63e33e68b09c480
Kvothek88 <115589647+Kvothek88@users.noreply.github.com>
2023-08-31 18:59:48 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,91 @@
+# Dictionary where 'Key:Value' pair is 'Plant name:Dvr credentials'
+PLANT_GROUP_1 = {"Plant 1":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 2":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 3":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 4":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 5":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 6":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 7":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+
+PLANT_GROUP_2 = {"Plant 8":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 9":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 10":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 11":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 12":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 13":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 14":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 15":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 16":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 17":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 18":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+
+PLANT_GROUP_3 = {"Plant 19":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 20":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 21":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 22":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 23":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 24":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+
+PLANT_GROUP_4 = {"Plant 25":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 26":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 27":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 28":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 29":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 30":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 31":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+
+PLANT_GROUP_5 = {"Plant 32":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 33":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 34":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 35":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 36":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 37":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+
+PLANT_GROUP_6 = {"Plant 38":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 39":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+                 "Plant 40":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+
+ALL_PLANTS = "Plant 1":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 2":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 3":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 4":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 5":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 6":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 7":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+             "Plant 8":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 9":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 10":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 11":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 12":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 13":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 14":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 15":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 16":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 17":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 18":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+             "Plant 19":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 20":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 21":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 22":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 23":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 24":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+             "Plant 25":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 26":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 27":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 28":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 29":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 30":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 31":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+             "Plant 32":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 33":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 34":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 35":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 36":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 37":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+             "Plant 38":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 39":{"url":"https://somesite.com/login","user":"admin","password":"????????"},
+             "Plant 40":{"url":"https://somesite.com/login","user":"admin","password":"????????"}}
+
+ALL_PLANTS_LIST = ["Plant 1","Plant 2",...,"Plant 40"]
+
+ALL_PLANT_LIST.sort()
