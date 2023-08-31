d265344aefcc0aca5aacd46a26b504f2295e2e0c
Kvothek88 <115589647+Kvothek88@users.noreply.github.com>
2023-08-31 18:51:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ import os
 from config import *
 
 # Function to capture screenshot
-def capture_dvr(plant_list,plant):
+def capture_dvr(plant_list,plant,delay):
     global offline_plants
     global blacklist
     if plant_list[plant]["gui"] != "ever" and plant not in blacklist:
 
         driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
         try:
-            driver.set_page_load_timeout(40)
+            driver.set_page_load_timeout(45)
             driver.get(plant_list[plant]["url"])
         except:
             offline_plants.append([plant])
@@ -30,22 +30,22 @@ def capture_dvr(plant_list,plant):
 
         driver.maximize_window()
 
-        time.sleep(10)
-
+        time.sleep(delay)
+ 
         username = driver.find_element(By.XPATH, '//input[@placeholder="User Name"]')
         username.send_keys(plant_list[plant]["user"])
         password = driver.find_element(By.XPATH, '//input[@placeholder="Password"]')
         password.send_keys(plant_list[plant]["password"])
         driver.find_element(By.XPATH, "//button").click()
 
-        time.sleep(25)
+        time.sleep(delay+5)
         
         if plant_list[plant]["gui"] == "hik-1":
             driver.find_element(By.XPATH, "//button[contains(@class, 'btn')]/i[contains(@class, 'icon-playall')]").click()
         elif plant_list[plant]["gui"] == "hik-2":
             driver.find_element(By.XPATH, "//button[@title='Start All Live View']").click()
 
-        time.sleep(10)
+        time.sleep(delay)
 
         ss_region = (0,0,1920,1080)
         ss_img = ImageGrab.grab(ss_region)
@@ -56,9 +56,10 @@ def capture_dvr(plant_list,plant):
     elif plant_list[plant]["gui"] != "ever" and plant in blacklist:
         driver = webdriver.Ie(service=Service(IEDriverManager().install()))          
         try:
-            driver.set_page_load_timeout(5)
+            driver.set_page_load_timeout(delay-5)
             driver.get(plant_list[plant]["url"])
         except:
+            time.sleep(delay)
             pyautogui.moveTo(1070,600,duration=1)
             pyautogui.click()
             pyautogui.typewrite(plant_list[plant]["user"],interval=0.1)
@@ -68,12 +69,14 @@ def capture_dvr(plant_list,plant):
             pyautogui.moveTo(1070,720,duration=1)
             pyautogui.click()
             driver.maximize_window()
-            time.sleep(5)
+            
+            time.sleep(delay+5)
+
             if plant_list[plant]["gui"] == "hik-1":
                 try:
                     driver.find_element(By.XPATH, "//button[contains(@class, 'btn')]/i[contains(@class, 'icon-playall')]").click()
                 except:
-                    time.sleep(5)
+                    time.sleep(delay-5)
                     ss_region = (0,0,1920,1080)
                     ss_img = ImageGrab.grab(ss_region)
                     ss_img.save(os.path.join(cwd,f"{folder_name}",f"{plant}.jpg"))
@@ -83,37 +86,50 @@ def capture_dvr(plant_list,plant):
                 try:
                     driver.find_element(By.XPATH, "//button[@title='Start All Live View']").click()
                 except:
-                    time.sleep(5)
+                    time.sleep(delay-5)
                     ss_region = (0,0,1920,1080)
                     ss_img = ImageGrab.grab(ss_region)
                     ss_img.save(os.path.join(cwd,f"{folder_name}",f"{plant}.jpg"))
                     pyautogui.moveTo(1900,30,duration=0.5)
-                    pyautogui.click()                     
+                    pyautogui.click()                   
     else:
         driver = webdriver.Ie(service=Service(IEDriverManager().install()))
         try:
-            driver.set_page_load_timeout(4)
+            driver.set_page_load_timeout(delay-5)
             driver.get(plant_list[plant]["url"])
         except:
             pyautogui.moveTo(550,700,duration=2)
             pyautogui.click()
-            time.sleep(5)
+            time.sleep(delay-5)
             driver.maximize_window()
-            time.sleep(7)
+            time.sleep(delay-3)
             ss_region = (0,0,1920,1080)
             ss_img = ImageGrab.grab(ss_region)
             ss_img.save(os.path.join(cwd,f"{folder_name}",f"{plant}.jpg")) 
-            driver.close() 
+            driver.close()       
+
 
 # Function to run capture_dvr for all plants in plant_list
 def run_capture_dvr(plant_list):
     global field
     global offline_plants
     global captured_plants
+    global delay
+
+    if type(plant_list) is str:
+        try:
+            plant_list = {plant_list:ALL_PLANTS[plant_list]}
+        except KeyError:
+            print("Choose a plant first")
 
     for plant in plant_list:
-        if plant not in captured_plants:
-            capture_dvr(plant_list,plant)
+        if plant not in captured_plants and [plant] not in captured_plants:
+            if plant in slow_plants:
+                capture_dvr(plant_list,plant,2*delay)
+            elif plant in very_slow_plants:
+                capture_dvr(plant_list,plant,4*delay)
+            else:
+                capture_dvr(plant_list,plant,delay)
             captured_plants.append([plant])
             file = open(os.path.join(cwd,f"{folder_name}","Captured Plants.csv"), 'a', newline ='')
             with file:
@@ -133,15 +149,20 @@ def get_variable_name(variable):
     for name in globals():
         if id(globals()[name]) == id(variable):
             return name
-
+        else:
+            return "General"
+        
 root = tk.Tk()
 root.title("DVR Screen Capture")
 
+current_plant = tk.StringVar(root)
+current_plant.set("-Choose Plant-")
+
 x = root.winfo_screenwidth()//2-250
 y = int(root.winfo_screenheight()*0.1)
-root.geometry('500x600+' + str(x) + '+' + str(y))
+root.geometry('540x750+' + str(x) + '+' + str(y))
 
-frame1 = tk.Frame(root, width=500, height=600, bg=bg_color)
+frame1 = tk.Frame(root, width=540, height=750, bg=bg_color)
 frame1.grid(row=0, column=0)
 frame1.pack_propagate(False)
 
@@ -228,11 +249,71 @@ button6 = tk.Button(
     activeforeground="black",
     command=lambda:run_capture_dvr(PLANT_GROUP_6))
 
+button7 = tk.Button(
+    frame1,
+    text="PLANT GROUP 7",
+    height=2,
+    width=15,
+    font=("TkHeadingFont",12),
+    bg="#28393a",
+    fg="white",
+    cursor="hand2",
+    activebackground="#badee2",
+    activeforeground="black",
+    command=lambda:run_capture_dvr(PLANT_GROUP_6))
+
+button8 = tk.Button(
+    frame1,
+    text="PLANT GROUP 7",
+    height=2,
+    width=15,
+    font=("TkHeadingFont",12),
+    bg="#28393a",
+    fg="white",
+    cursor="hand2",
+    activebackground="#badee2",
+    activeforeground="black",
+    command=lambda:run_capture_dvr(PLANT_GROUP_6))
+
+button9 = tk.Button(
+    frame1,
+    text="CAPTURE PLANT",
+    height=2,
+    width=15,
+    font=("TkHeadingFont",12),
+    bg="#4f6457",
+    fg="white",
+    cursor="hand2",
+    activebackground="#badee2",
+    activeforeground="black",
+    command=lambda:run_capture_dvr(current_plant.get()))
+
+button10 = tk.Button(
+    frame1,
+    text="CAPTURE ALL",
+    height=2,
+    width=15,
+    font=("TkHeadingFont",12),
+    bg="#4f6457",
+    fg="white",
+    cursor="hand2",
+    activebackground="#badee2",
+    activeforeground="black",
+    command=lambda:run_capture_dvr(ALL_PLANTS))
+
 button1.place(x=65,y=200)
-button2.place(x=290,y=200)
+button2.place(x=330,y=200)
 button3.place(x=65,y=280)
-button4.place(x=290,y=280)
+button4.place(x=330,y=280)
 button5.place(x=65,y=360)
-button6.place(x=290,y=360)
+button6.place(x=330,y=360)
+button7.place(x=65,y=440)
+button8.place(x=330,y=440)
+button9.place(x=65,y=630)
+button10.place(x=65,y=550)
+
+menu = tk.OptionMenu(frame1, current_plant, *ALL_PLANTS_LIST)
+menu.config(bg="#4f6457",fg="white",activebackground="#badee2",activeforeground="black",font=("TkHeadingFont",12),highlightthickness=0)
+menu.place(x=245,y=645)
 
 root.mainloop()
